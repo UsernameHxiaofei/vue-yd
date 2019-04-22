8b3e49d18d5d4c17b8449c7b6c21e146c78441ac
hujunfei <1253179427@qq.com>
2019-04-22 10:56:30 +0800

    项目构架

---
+++ src/page/bespeak/bespeak.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="subject">
+        <som-nav-bar  left-text=" " right-text="预约记录" class="NavBar" @on-right-click="Record">养修预约</som-nav-bar>
+        <FormBoby/>
+    </div>
+</template>
+<script>
+import FormBoby from './FormBoby' //导入页面组件
+export default {
+  name: 'bespeak',
+  components:{
+    FormBoby
+  },
+  methods:{
+    Record(){ //跳转到预约记录页面
+      this.$router.push({path:'/AppointmentRecord'})
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .subject{
+    .NavBar{
+      color: #666
+    }
+  }
+</style>
